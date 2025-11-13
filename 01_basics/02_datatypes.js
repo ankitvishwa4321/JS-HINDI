09c8dfafb5a254c1b266e75de5cd723e5f85cbfb
Ankit Vishwakarma <ankitvishwa4321@gmail.com>
2025-11-13 11:11:55 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all  code as newer version JS CODE
+
+// alert( 3 + 8) // we are using nodejs, not  a browser
+
+console.log(3  +  8) // code readability should be high
+
+console.log("Vishwa")
+
+const employeeId = 1
+let employeename = "VISHWA"
+let employeeage = 18
+let employeeisLoggedIn = true
+let employeestate = "Maharashtra";
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // the type of undefined is same undefined
+console.log(typeof null); // the type of  null is object
+console.table([employeeId,employeename,employeeage,employeeisLoggedIn,employeestate])
